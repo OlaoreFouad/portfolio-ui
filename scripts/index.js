11ee0d4cba3ca4baf2aee93e0dffe38d1e9234c3
Foodie <olaorefouad565@gmail.com>
2019-12-22 18:38:10 +0100

    ui: the ui is complete... finally!

---
+++ scripts/index.js
@@ -0,0 +1,12 @@
+
+registerModal()
+
+function registerModal() {
+    const modal = document.getElementById('modal');
+    modal.style.display = 'block';
+
+    // window.onclick = () => {
+    //     modal.style.display = 'none';
+    // }
+
+}
