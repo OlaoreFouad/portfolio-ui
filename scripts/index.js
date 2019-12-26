1824e7fa5f3d5a273ab166a1da6515b82dc5e58b
Foodie <olaorefouad565@gmail.com>
2019-12-26 13:30:53 +0100

    made small changes to the modal, visibility: hidden was the cause...

--- scripts/index.js
+++ scripts/index.js
@@ -5,8 +5,8 @@ function registerModal() {
     const modal = document.getElementById('modal');
     modal.style.display = 'block';
 
-    // window.onclick = () => {
-    //     modal.style.display = 'none';
-    // }
+    window.onclick = () => {
+        modal.style.display = 'none';
+    }
 
 }
